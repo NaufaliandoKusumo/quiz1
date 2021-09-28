2546cd16d99a797f0f7b19e2d90c8f55ea896478
NaufaliandoKusumo <77374896+NaufaliandoKusumo@users.noreply.github.com>
2021-09-29 00:11:48 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 {
-    const display27610 = document.getElementById("clock");
+    const displayclock = document.getElementById("clock");
 
     // conver number num to string, pad 0 in front to make it 2 digits
     const padNum = ((num) => num.toString(). padStart (2 , "0"));
@@ -9,7 +9,7 @@
         const hh = dd.getHours();
         const mm = dd.getMinutes();
         const ss = dd.getSeconds();
-        display27610.textContent = `${padNum(hh)}:${padNum(mm)}:${padNum(ss)}`;
+        displayclock.textContent = `${padNum(hh)}:${padNum(mm)}:${padNum(ss)}`;
     });
     
     setInterval(updateDisplay, 1000);
